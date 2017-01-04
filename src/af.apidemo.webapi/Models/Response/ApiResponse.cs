b6686e5081e735ac43e2323c494477604d8bfc39
Robert Mayer <rwlmayer@gmail.com>
2017-01-04 08:29:45 +0100

    Added ApiResponse class

---
+++ src/af.apidemo.webapi/Models/Response/ApiResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace af.apidemo.webapi.Models.Response
+{
+    public class ApiResponse<T>
+    {
+        public ApiResponse()
+        {
+
+        }
+        public ApiResponse(T obj)
+        {
+            Data = obj;
+        }
+        public ApiResponse(int code, string message)
+        {
+            Code = code;
+            Message = message;
+        }
+        public T Data { get; set; }
+        public int? Code { get; set; }
+        public string Message { get; set; }
+    }
+}
