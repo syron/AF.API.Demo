35fb6f7c90f0585bde58a8ebc0d3ca1774aeb852
Robert Mayer <rwlmayer@gmail.com>
2017-01-04 08:26:56 +0100

    Added response classes

---
+++ src/af.apidemo.webapi/Models/Response/Error.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace af.apidemo.webapi.Models.Response
+{
+    public class Error
+    {
+        public Error() { }
+        public Error(int code, string message)
+        {
+            this.Code = code;
+            this.Message = message;
+        }
+
+        public int Code { get; set; }
+        public string Message { get; set; }
+    }
+}
